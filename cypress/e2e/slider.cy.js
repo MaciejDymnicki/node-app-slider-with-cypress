d6c4648bb46b42a43c3ebd21c8260f6e3128798e
MaciejDymnicki <maciek.dymnicki@gmail.com>
2024-05-29 22:37:24 +0200

    Hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,26 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+//Test 2: Zweryfikuj, czy opis każdego slajdu jest wyświetlany poprawnie
+describe('Swiper Gallery Slide Content Test', function () {
+  const slides = [
+    { title: 'Rome', description: 'Italy' },
+    { title: 'London', description: 'United Kingdom' },
+    { title: 'Paris', description: 'France' }
+  ];
+
+   it('should display correct title and description for each slide', function () {
+    cy.visit('http://localhost:3000');
+    
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(1000); // Czekaj na zakończenie animacji przejścia slajdu
+      }
+      cy.get('.swiper-slide-active h1').should('contain', slide.title);
+      cy.get('.swiper-slide-active p').should('contain', slide.description);
+    });
+  });
+});
+
